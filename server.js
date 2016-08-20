9d5ed8c3210321e1f1ac5dcfd0b943d01957331e
shainaveloz <shaina.veloz@gmail.com>
2016-08-20 12:44:33 -0400

    added index.html

---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+var path = require('path');
+var passport = require('passport');
+var session  = require('express-session');
+var cookieParser = require('cookie-parser');
+
+app.use(express.static(__dirname + '/public'));
+
+// BodyParser interprets data sent to the server
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+// cookie parser for user authentication
+app.use(cookieParser());
+// session configuration
+app.use(session({
+    secret: 'resticket',
+    cookie: { maxAge: 100000 },
+    resave: true,
+    saveUninitialized: true,
+} ));
+
+// use passport authentication middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function(req, res) {
+        res.redirect('/');
+    });
+
+var PORT = process.env.PORT || 8080;
+
+app.listen(PORT, function() {
+    console.log("Listening on %d", PORT);
+});
